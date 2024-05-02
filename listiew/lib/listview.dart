1e7ea8535cc39a600af64fcb227afd5ff0c4a5e6
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-05-02 15:40:15 +0500

    passing the values to listtile, & itemcount using List

--- listiew/lib/listview.dart
+++ listiew/lib/listview.dart
@@ -8,18 +8,19 @@ class listView extends StatefulWidget {
 }
 
 class _listViewState extends State<listView> {
+  List students = ["Ali", "Ahmed", "Saad", "Asad", "Waqas", "Essa", "Sadaf"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
             child: ListView.builder(
-      itemCount: 6,
+      itemCount: students.length,
       itemBuilder: (context, index) {
         return Container(
           margin: EdgeInsets.only(bottom: 2),
           child: ListTile(
             tileColor: Colors.amber[100],
-            title: Text(index.toString()),
+            title: Text(students[index]),
           ),
         );
       },
