49a0d7421f7027c375d53cc1c228937cd10c8811
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-05-02 15:44:26 +0500

    create elevated button in appbar to add values in List view

--- listiew/lib/listview.dart
+++ listiew/lib/listview.dart
@@ -12,18 +12,24 @@ class _listViewState extends State<listView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: ElevatedButton(
+            onPressed: () {},
+            child: Text("Add Value"),
+          ),
+        ),
         body: SafeArea(
             child: ListView.builder(
-      itemCount: students.length,
-      itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.only(bottom: 2),
-          child: ListTile(
-            tileColor: Colors.amber[100],
-            title: Text(students[index]),
-          ),
-        );
-      },
-    )));
+          itemCount: students.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 2),
+              child: ListTile(
+                tileColor: Colors.amber[100],
+                title: Text(students[index]),
+              ),
+            );
+          },
+        )));
   }
 }
